aa30e3507368392586437df5f1a8e178e8764f78
Roman Maliushkin <malyushkinr@gmail.com>
2022-07-24 16:01:57 +0200

    Diplom restructure

--- devops-diplom-yandexcloud/terraform/main.tf
+++ devops-diplom-yandexcloud/terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  cloud {
+    organization = "maliushkin"
+
+    workspaces {
+      name = "devops-diplom-yandexcloud"
+    }
+  }
+}
+
 resource "yandex_compute_instance" "nat_instance" {
   name     = "nat"
   hostname = "nat.maliushkin.ru"
@@ -27,7 +37,6 @@ resource "yandex_compute_instance" "nat_instance" {
 
 resource "yandex_compute_instance" "entrance_instance" {
   name = "main"
-  # hostname = "maliushkin.ru"
   zone = "ru-central1-a"
 
   resources {
@@ -37,7 +46,7 @@ resource "yandex_compute_instance" "entrance_instance" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd81d2d9ifd50gmvc03g" # fd8ad4ie6nhfeln6bsof centos
+      image_id = "fd81d2d9ifd50gmvc03g"
       size     = 10
     }
   }
@@ -54,7 +63,6 @@ resource "yandex_compute_instance" "entrance_instance" {
 
 resource "yandex_compute_instance" "db01_instance" {
   name = "mysql-master"
-  # hostname = "db01.maliushkin.ru"
   zone = "ru-central1-a"
 
   resources {
@@ -64,7 +72,8 @@ resource "yandex_compute_instance" "db01_instance" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd81d2d9ifd50gmvc03g" # fd8ad4ie6nhfeln6bsof centos
+      image_id = "fd81d2d9ifd50gmvc03g"
+      size     = 10
     }
   }
 
@@ -79,7 +88,6 @@ resource "yandex_compute_instance" "db01_instance" {
 
 resource "yandex_compute_instance" "db02_instance" {
   name = "mysql-slave"
-  # hostname = "db02.maliushkin.ru"
   zone = "ru-central1-a"
 
   resources {
@@ -89,7 +97,8 @@ resource "yandex_compute_instance" "db02_instance" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd81d2d9ifd50gmvc03g" # fd8ad4ie6nhfeln6bsof centos
+      image_id = "fd81d2d9ifd50gmvc03g"
+      size     = 10
     }
   }
 
@@ -104,7 +113,6 @@ resource "yandex_compute_instance" "db02_instance" {
 
 resource "yandex_compute_instance" "app_instance" {
   name = "wordpress"
-  # hostname = "app.maliushkin.ru"
   zone = "ru-central1-a"
 
   resources {
@@ -114,7 +122,7 @@ resource "yandex_compute_instance" "app_instance" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd81d2d9ifd50gmvc03g" # fd8ad4ie6nhfeln6bsof centos
+      image_id = "fd81d2d9ifd50gmvc03g"
       size     = 10
     }
   }
@@ -130,7 +138,6 @@ resource "yandex_compute_instance" "app_instance" {
 
 resource "yandex_compute_instance" "monitoring_instance" {
   name = "monitoring"
-  # hostname = "monitoring.maliushkin.ru"
   zone = "ru-central1-a"
 
   resources {
@@ -140,7 +147,57 @@ resource "yandex_compute_instance" "monitoring_instance" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd81d2d9ifd50gmvc03g" # fd8ad4ie6nhfeln6bsof centos
+      image_id = "fd81d2d9ifd50gmvc03g"
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.private_vpc_subnet.id
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "gitlab_instance" {
+  name = "gitlab"
+  zone = "ru-central1-a"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd81d2d9ifd50gmvc03g"
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.private_vpc_subnet.id
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "runner_instance" {
+  name = "runner"
+  zone = "ru-central1-a"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd81d2d9ifd50gmvc03g"
       size     = 10
     }
   }
